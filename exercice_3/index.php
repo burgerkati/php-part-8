be82c2a27207e10a9d98ab3f9393fe29512a1384
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-24 10:37:17 +0200

    add 3rd exercise with comment of output

---
+++ exercice_3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP pt.8 exercice_3</title>
+  </head>
+  <body>
+  <?php
+  // prints day of the week day month and year
+  echo "Today's date is: " . date('l jS \of F Y');
+  ?>
+  </body>
+</html>
