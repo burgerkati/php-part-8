037c7cbfd1164e8b2ae9adf2d8f629a6530a051c
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-24 14:55:29 +0200

    add finished exercise w/o style

---
+++ exercice_5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP pt.8 exercice_5</title>
+  </head>
+  <!-- Afficher le timestamp courant et afficher le timestamp du 1er février 2021. -->
+  <body>
+  <?php
+  // set default timezone
+  date_default_timezone_set("Europe/Berlin");
+  // create current timestamp
+  $date=date_create();
+  date_timestamp_get($date);
+  // output of current timestamp
+  echo "The current timestamp is" . date_format($date, "U = Y-m-d") . "<br>";
+  // change current timestamp to 1st of February 2021 from 24th of June 2021
+  $date=strtotime("-21 Weeks + 4 Days");
+  // output of current timestamp transformation to 1st of February 2021
+  echo date("Y-m-d", $date);
+  ?>
+  </body>
+</html>
